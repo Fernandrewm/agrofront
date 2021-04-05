adb7b8e6c251778614ee8a000801e97a7034ccea
Fernando Andrés Muñoz Suazo <fernandrewm@gmail.com>
2021-04-05 14:53:36 -0600

    Adding dynamic styles to pages through classNames in basic-layout

---
+++ pages/account.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import BasicLayout from "../layouts/BasicLayout";
+
+export default function account() {
+    return (
+        <BasicLayout className="account">
+            <h1>Estamos en una cuenta de usuario</h1>
+        </BasicLayout>
+    )
+}
