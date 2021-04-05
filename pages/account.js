565686abed5e8e43eb522b82eaa37e7c0b61c2ae
Fernando Andrés Muñoz Suazo <fernandrewm@gmail.com>
2021-04-05 16:37:53 -0600

    Launching the user's configuration section

--- pages/account.js
+++ pages/account.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, {useState, useEffect} from "react";
+import {useRouter} from "next/router";
 import BasicLayout from "../layouts/BasicLayout";
+import {getMeApi} from "../api/user";
+import useAuth from "../hooks/useAuth";
 
 export default function account() {
+    const [user, setUser] = useState(undefined);
+    const {auth, logout} = useAuth();
+    const router = useRouter();
+
+    //Comprobamos si el usuario esta logueado o no (undefined)
+    useEffect(() => {
+        (async () => {
+            const response = await getMeApi(logout);
+            setUser(response || null);
+        })()
+    }, [auth]);
+
+    if(user === undefined) return null;
+
+    //Lo devolvemos a la home
+    if(!auth && !user){
+        router.replace("/");
+    }
+
+    //En caso de estar logueado mostramos la pagina
     return (
         <BasicLayout className="account">
-            <h1>Estamos en una cuenta de usuario</h1>
+            <Configuration/>
         </BasicLayout>
+    )
+}
+
+function Configuration(){
+    return(
+        <div className="account__configuration">
+            <div className="title">Configuracion</div>
+            <div className="data">Formularios de configuracion</div>
+        </div>
     )
 }
