7b688f8d05c1e85c754a4e2edd682fa466f8c3d6
Fernando Andrés Muñoz Suazo <fernandrewm@gmail.com>
2021-04-19 13:53:43 -0600

    Showing products by category

--- api/product.js
+++ api/product.js
@@ -13,3 +13,18 @@ export async function getLastProductsApi(limit) {
       return null;
     }
   }
+
+export async function getProductsCategoryApi(category, limit, start) {
+  try {
+    const limitItems = `_limit=${limit}`;
+    const sortItems = `_sort=created_at:desc`;
+    const startItems = `_start=${start}`;
+    const url = `${BASE_PATH}/products?category.url=${category}&${limitItems}&${sortItems}&${startItems}`;
+    const response = await fetch(url);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
--- pages/categorias/[category].js
+++ pages/categorias/[category].js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {Loader} from "semantic-ui-react";
 import {useRouter} from "next/router";
+import {size} from "lodash";
 import BasicLayout from "../../layouts/BasicLayout";
+import {getProductsCategoryApi} from "../../api/product";
+import ListProducts from "../../components/ListProducts";
+
+const limitPerPage = 10;
 
 export default function Category() {
     const {query} = useRouter();
 
+    const [products, setProducts] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            if (query.category){
+                const response = await getProductsCategoryApi(query.category, limitPerPage, 0);
+                setProducts(response);
+            }
+        })()
+    }, [query]);
+
     return (
         <BasicLayout className="category">
-            <h1>Estamos en categoria: {query.category}</h1>
+            {!products && <Loader active>Cargando productos...</Loader>}
+            {products && size(products) === 0 && (
+                <div>
+                    <h3>No hay productos.</h3>
+                </div>
+            )}
+            {size(products) > 0 && (
+                <ListProducts products={products}/>
+            )}
         </BasicLayout>
     )
 }
