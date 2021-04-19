1329722497842ef048549a19ef72062348404287
Fernando Andrés Muñoz Suazo <fernandrewm@gmail.com>
2021-04-19 11:45:15 -0600

    Responsive ListProducts

--- components/ListProducts/ListProducts.js
+++ components/ListProducts/ListProducts.js
@@ -2,14 +2,31 @@ import React from 'react';
 import {Image, Grid} from "semantic-ui-react";
 import Link from "next/link";
 import {map} from "lodash";
+import useWindowSize from "../../hooks/useWindowSize";
+import {breakpointUpSm, breakpointUpMd, breakpointUpLg} from "../../utils/breakpoint";
 
 export default function ListProducts(props) {
     const {products} = props;
+    const {width} = useWindowSize();
+
+    //Calcula cuantas columnas va renderizar en cada size
+    const getColumnsRender = () => {
+        switch (true) {
+            case width > breakpointUpLg:
+                return 5;
+            case width > breakpointUpMd:
+                return 3;
+            case width > breakpointUpSm:
+                return 2;
+            default:
+                return 1;
+        }
+    }
 
     return (
         <div className="list-products">
             <Grid>
-                <Grid.Row columns={5}>
+                <Grid.Row columns={getColumnsRender()}>
                     {map(products, (product) => (
                         <Product product={product}/>
                     ))}
