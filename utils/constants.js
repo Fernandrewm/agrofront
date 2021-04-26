ed6541cd3959fb7cc2b80bcb45e6ed93c5a6e204
Fernando Andrés Muñoz Suazo <fernandrewm@gmail.com>
2021-04-26 15:16:41 -0600

    Change directions of backend

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,4 @@
-export const BASE_PATH = "https://agroback-heroku.herokuapp.com"
+export const BASE_PATH = "http://104.236.219.83:1337"
+// export const BASE_PATH = "https://agroback-heroku.herokuapp.com"
 // export const BASE_PATH = "http://localhost:1337"
 export const TOKEN = "token";
