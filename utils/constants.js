4ea2d9878fcf975a345af1eabeece36672a10daf
Fernando Andrés Muñoz Suazo <fernandrewm@gmail.com>
2021-04-27 16:02:45 -0600

    DigitalOcean

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_PATH = "http://104.236.219.83:1337"
+export const BASE_PATH = "https://agroapp-backend-mot2i.ondigitalocean.app"
 // export const BASE_PATH = "https://agroback-heroku.herokuapp.com"
 // export const BASE_PATH = "http://localhost:1337"
 export const TOKEN = "token";
