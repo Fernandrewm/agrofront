b1a842d23567a959c82a733b6b3a5655d15b10cf
Fernando Andrés Muñoz Suazo <fernandrewm@gmail.com>
2021-05-04 16:08:36 -0600

    Products page by category ready to use.

--- pages/categorias/[category].js
+++ pages/categorias/[category].js
@@ -5,6 +5,7 @@ import {size} from "lodash";
 import BasicLayout from "../../layouts/BasicLayout";
 import {getProductsCategoryApi, getTotalProductCategoryApi} from "../../api/product";
 import ListProducts from "../../components/ListProducts";
+import Pagination from "../../components/Pagination";
 
 const limitPerPage = 10;
 
@@ -13,6 +14,7 @@ export default function Category() {
     const [products, setProducts] = useState(null);
     const [totalProducts, setTotalProducts] = useState(null);
 
+    //We indicate from which product number to start (it is used for pagination).
     const getStartItem = () => {
         const currentPages = parseInt(query.page);
         if (!query.page || currentPages === 1) return 0;
@@ -20,15 +22,17 @@ export default function Category() {
     }
     console.log(getStartItem());
 
+    //We obtain all products by category
     useEffect(() => {
         (async () => {
             if (query.category){
-                const response = await getProductsCategoryApi(query.category, limitPerPage, 0);
+                const response = await getProductsCategoryApi(query.category, limitPerPage, getStartItem());
                 setProducts(response);
             }
         })()
     }, [query]);
 
+    //We obtain the total number of products per category (it is used for pagination).
     useEffect(() => {
         ( async () => {
             const response = await getTotalProductCategoryApi(query.category);
@@ -47,6 +51,14 @@ export default function Category() {
             {size(products) > 0 && (
                 <ListProducts products={products}/>
             )}
+
+            {totalProducts ? (
+                <Pagination 
+                    totalProducts={totalProducts} 
+                    page={query.page ? parseInt(query.page) : 1}
+                    limitPerPage={limitPerPage}
+                />
+            ) : null}
         </BasicLayout>
     )
 }
