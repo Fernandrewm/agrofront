6629c2118b1ecce930eae6b6fbb9f2ccf85032c1
Fernando Andrés Muñoz Suazo <fernandrewm@gmail.com>
2021-05-21 14:59:05 -0600

    Implemented favorite product add and delete

--- components/ListProducts/ListProducts.js
+++ components/ListProducts/ListProducts.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import {Image, Grid} from "semantic-ui-react";
+import React, {useState, useEffect} from 'react';
+import {Image, Grid, Icon, GridColumn} from "semantic-ui-react";
 import Link from "next/link";
-import {map} from "lodash";
+import {map, size} from "lodash"; 
 import useWindowSize from "../../hooks/useWindowSize";
 import {breakpointUpSm, breakpointUpMd, breakpointUpLg} from "../../utils/breakpoint";
+import useAuth from "../../hooks/useAuth";
+import {isFavoriteApi, addFavoriteApi, deleteFavoriteApi} from "../../api/favorite";
 
 export default function ListProducts(props) {
     const {products} = props;
@@ -38,14 +40,49 @@ export default function ListProducts(props) {
 
 function Product(props) {
     const {product} = props;
+    const {auth, logout} = useAuth();
+
+    const [isFavorite, setIsFavorite] = useState(false);
+    const [reloadFavorite, setReloadFavorite] = useState(false);
+    
+    useEffect(() => {
+        (async () => {
+            const response = await isFavoriteApi(auth.idUser, product.id, logout);
+            if(size(response) > 0) setIsFavorite(true);
+            else setIsFavorite(false);
+        })();
+        setReloadFavorite(false);
+    }, [product, reloadFavorite]);
+
+    const addFavorite = async () => {
+        if(auth){
+            await addFavoriteApi(auth.idUser, product.id, logout);
+            setReloadFavorite(true);
+        }
+    }
+
+    const deleteFavorite = async () => {
+        if(auth){
+            await deleteFavoriteApi(auth.idUser, product.id, logout);
+            setReloadFavorite(true);
+        }
+    }
+
     return (
         <Grid.Column className="list-products__product">
-            <Link href={`/${product.url}`}>
+            {/* <Link href={`/${product.url}`}> */}
                 <a>
                     <div className="list-products__product-image">
                         <Image src={product.image.url} alt={product.title}/>
                         <div className="list-products__product-info">
-                            <h2>{product.title}</h2>
+                            <div className="list-products__product-info-header">
+                                <h2>{product.title}</h2>
+                                <Icon 
+                                    name={isFavorite ? "heart" : "heart outline"}
+                                    link
+                                    onClick = {isFavorite ? deleteFavorite : addFavorite}
+                                />
+                            </div>
                             {/* Si el producto tiene descuento se muestra * */}
                             <div className="list-products__product-price">
                                 {product.discount ? (
@@ -58,7 +95,7 @@ function Product(props) {
                         </div>
                     </div>
                 </a>
-            </Link>
+            {/* </Link> */}
         </Grid.Column>
     )
 }
