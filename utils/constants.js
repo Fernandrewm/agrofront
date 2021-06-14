63a7faca9d960d6d789581b86c6aea98a214d97a
Fernando Andrés Muñoz Suazo <fernandrewm@gmail.com>
2021-06-14 13:37:53 -0600

    I always forget to change the origin when I switch from dev to prod.

--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,6 @@
-// export const BASE_PATH = "https://agroapp-backend-mot2i.ondigitalocean.app"
+export const BASE_PATH = "https://agroapp-backend-mot2i.ondigitalocean.app"
 // export const BASE_PATH = "https://agroback-heroku.herokuapp.com"
-export const BASE_PATH = "http://localhost:1337"
+// export const BASE_PATH = "http://localhost:1337"
 export const TOKEN = "token";
 export const CART = "cart";
 export const ISV = 1.15;
